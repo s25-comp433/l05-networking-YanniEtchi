c0ae418ba7131cb8c6872f5a307baa711237f765
Yanni Etchi <81278130+YanniEtchi237@users.noreply.github.com>
2025-03-03 10:53:32 -0500

    swift format fix

--- iTunesSearch/iTunesSearch/ContentView.swift
+++ iTunesSearch/iTunesSearch/ContentView.swift
@@ -19,37 +19,43 @@ struct Result: Codable {
 
 struct ContentView: View {
     @State private var results = [Result]()
-    
+
     var body: some View {
-        List(results, id: \.trackId){item in
-            VStack(alignment: .leading){
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
                 Text(item.trackName)
                     .font(.headline)
-                
+
                 Text(item.collectionName)
-                    
+
             }
         }
-        .task{
+        .task {
             await loadData()
         }
     }
-    
-    
-    func loadData()  async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+
+    func loadData() async {
+        guard
+            let url = URL(
+                string:
+                    "https://itunes.apple.com/search?term=taylor+swift&entity=song"
+            )
+        else {
             print("invalid URL")
             return
         }
-        
-        do{
+
+        do {
             let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data){
-                
+
+            if let decodedResponse = try? JSONDecoder().decode(
+                Response.self, from: data)
+            {
+
                 results = decodedResponse.results
             }
-        }catch{
+        } catch {
             print("invalid data")
         }
     }
